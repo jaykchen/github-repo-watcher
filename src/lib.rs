6e5bc8129149b72bfda467152056f4721aa92053
jaykchen@gmail.com <jaykchen@gmail.com>
2024-01-06 10:11:47 -0500

    examine stars, http_req

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ use csv::{QuoteStyle, WriterBuilder};
 use dotenv::dotenv;
 use flowsnet_platform_sdk::logger;
 use github_flows::{get_octo, GithubLogin};
-use octocrab_wasi::models::pulls::Base;
 use schedule_flows::{schedule_cron_job, schedule_handler};
 use serde::{Deserialize, Serialize};
 use std::{collections::HashSet, env};
@@ -231,9 +230,9 @@ async fn track_stargazers(
     }
 
     let mut after_cursor: Option<String> = None;
-    let octocrab = get_octo(&GithubLogin::Provided(
-        env::var("GITHUB_TOKEN").expect("github_token is required"),
-    ));
+    // let octocrab = get_octo(&GithubLogin::Provided(
+    //     env::var("GITHUB_TOKEN").expect("github_token is required"),
+    // ));
 
     for _n in 1..99 {
         let query_str = format!(
@@ -262,68 +261,62 @@ async fn track_stargazers(
                 .map_or("null".to_string(), |cursor| format!(r#""{}""#, cursor))
         );
 
-        let token = env::var("GITHUB_TOKEN").expect("github_token is required");
-        let base_url = "https://api.github.com/graphql";
+        let mut response: GraphQLResponse = GraphQLResponse { data: None };
 
-        match github_http_post(&token, base_url, &query_str).await {
-            Some(response) => {
-                let text = String::from_utf8_lossy(&response);
-                log::info!("GraphQL response: {}", text);
-                // Add logic to update `after_cursor` if pagination is needed
-                // ...
+        match github_http_post_gql(&query_str).await {
+            Ok(r) => {
+                response = match serde_json::from_slice::<GraphQLResponse>(&r) {
+                    Ok(res) => res,
+                    Err(err) => {
+                        log::error!("Failed to deserialize response from Github: {}", err);
+                        continue;
+                    }
+                };
             }
-            None => {
-                log::error!("Error making GraphQL request: ");
-                break;
-            }
+            Err(_e) => continue,
         }
-        // let response: GraphQLResponse = octocrab.graphql(&query_str).await?;
-        // let stargazers = response
-        //     .data
-        //     .and_then(|data| data.repository)
-        //     .and_then(|repo| repo.stargazers);
+        let stargazers = response
+            .data
+            .and_then(|data| data.repository)
+            .and_then(|repo| repo.stargazers);
 
         // log::info!("stargazers: {:?}", stargazers);
 
-        return Ok(());
-        // if let Some(stargazers) = stargazers {
-        //     for edge in stargazers.edges.unwrap_or_default() {
-        //         if let Some(node) = edge.node {
-        //             let login = node.login.clone().unwrap_or_default();
-        //             let is_watching = match found_set.contains(&login) {
-        //                 true => String::from("Yes"),
-        //                 false => String::from(""),
-        //             };
-        //             if !node.email.is_empty() {
-        //                 log::info!("{} has email {}", login, node.clone().email);
-        //             }
+        if let Some(stargazers) = stargazers {
+            for edge in stargazers.edges.unwrap_or_default() {
+                if let Some(node) = edge.node {
+                    let login = node.login.clone().unwrap_or_default();
+                    let is_watching = match found_set.contains(&login) {
+                        true => String::from("Yes"),
+                        false => String::from(""),
+                    };
 
-        //             if let Err(err) = wtr.write_record(&[
-        //                 login,
-        //                 node.email,
-        //                 node.twitterUsername.unwrap_or("".to_string()),
-        //                 is_watching,
-        //             ]) {
-        //                 log::error!("Failed to write record: {:?}", err);
-        //             }
-        //         }
-        //     }
-        //     wtr.flush()?;
+                    if let Err(err) = wtr.write_record(&[
+                        login,
+                        node.email.unwrap_or("".to_string()),
+                        node.twitterUsername.unwrap_or("".to_string()),
+                        is_watching,
+                    ]) {
+                        log::error!("Failed to write record: {:?}", err);
+                    }
+                }
+            }
+            wtr.flush()?;
 
-        //     if let Some(page_info) = stargazers.page_info {
-        //         if page_info.has_next_page.unwrap_or(false) {
-        //             after_cursor = page_info.end_cursor;
-        //         } else {
-        //             log::info!("stargazers loop {}", _n);
-        //             break;
-        //         }
-        //     } else {
-        //         log::error!("pageInfo is missing from the response");
-        //         break;
-        //     }
-        // } else {
-        //     break;
-        // }
+            if let Some(page_info) = stargazers.page_info {
+                if page_info.has_next_page.unwrap_or(false) {
+                    after_cursor = page_info.end_cursor;
+                } else {
+                    log::info!("stargazers loop {}", _n);
+                    break;
+                }
+            } else {
+                log::error!("pageInfo is missing from the response");
+                break;
+            }
+        } else {
+            break;
+        }
     }
 
     Ok(())
@@ -459,9 +452,10 @@ pub async fn upload_to_gist(wtr: csv::Writer<Vec<u8>>) -> anyhow::Result<()> {
     Ok(())
 }
 
-pub async fn github_http_post(token: &str, base_url: &str, query: &str) -> Option<Vec<u8>> {
-    use http_req::{request::Method, request::Request, response, uri::Uri};
-
+pub async fn github_http_post_gql(query: &str) -> anyhow::Result<Vec<u8>> {
+    use http_req::{request::Method, request::Request, uri::Uri};
+    let token = env::var("GITHUB_TOKEN").expect("github_token is required");
+    let base_url = "https://api.github.com/graphql";
     let base_url = Uri::try_from(base_url).unwrap();
     let mut writer = Vec::new();
 
@@ -478,13 +472,13 @@ pub async fn github_http_post(token: &str, base_url: &str, query: &str) -> Optio
         Ok(res) => {
             if !res.status_code().is_success() {
                 log::error!("Github http error {:?}", res.status_code());
-                return None;
+                return Err(anyhow::anyhow!("Github http error {:?}", res.status_code()));
             };
-            Some(writer)
+            Ok(writer)
         }
         Err(_e) => {
             log::error!("Error getting response from Github: {:?}", _e);
-            None
+            Err(anyhow::anyhow!(_e))
         }
     }
 }
